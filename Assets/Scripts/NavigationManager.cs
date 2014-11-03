2fe529a6def3cbaba45dd6c76116634ac7eade7a
ZhaoshengWang <wang.zhaosheng@hotmail.com>
2014-11-02 21:41:41 -0500

    beginning

---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationManager : MonoBehaviour {
+
+	public void sceneToChange (string sceneToChange) {
+		Application.LoadLevel (sceneToChange);
+	}
+}
