998b061fa606db9e10df49b0e56b83324a38c0b3
Anne <A.C.Anne.Chen@gmail.com>
2014-11-09 16:57:57 -0500

    Firing objects from enemy side of the plane.

---
+++ Assets/EnemyLivestockMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLivestockMovement : MonoBehaviour 
+{
+	Transform firingSpot;
+	NavMeshAgent nav;
+
+	void Awake()
+	{
+		firingSpot = GameObject.FindGameObjectWithTag("FiringSpot").transform;
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		nav.SetDestination(firingSpot.position);
+	}
+	
+}
