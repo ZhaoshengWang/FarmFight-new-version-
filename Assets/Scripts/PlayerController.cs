402bed013ab51f221dbf14c2894869d3dd19afbb
ZhaoshengWang <wang.zhaosheng@hotmail.com>
2014-11-08 23:40:30 -0500

    Add camera(s)  press f1 and f2 and minimap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,22 @@ public class PlayerController : MonoBehaviour {
 	public float turnSmoothValue = 15f;
 	public float speedDampTime = 0.1f;
 	public float speed;
+	public GameObject FirstPersonViewCamera;
+	public GameObject ThirdPersonViewCamera;
 	//private Animator anim;
 	
 
 	void FixedUpdate(){
+		if (Input.GetKey (KeyCode.F1)) {
+			ThirdPersonViewCamera.SetActive(false);
+			FirstPersonViewCamera.SetActive(true);
+
+		}
+		if (Input.GetKey (KeyCode.F2)) {
+			FirstPersonViewCamera.SetActive(false);
+			ThirdPersonViewCamera.SetActive(true);
+			
+		}
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 		if (h == 0f && v == 0f) {
@@ -24,7 +36,8 @@ public class PlayerController : MonoBehaviour {
 						//anim.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
 						Vector3 movement = new Vector3 (h, 0.0f, v);
 						rigidbody.AddForce (movement * speed * Time.deltaTime);
-			
+						//CharacterController cc = GetComponent<CharacterController> ();
+						//cc.SimpleMove (movement);
 		
 	}
 	void DoRotate(float h, float v){
