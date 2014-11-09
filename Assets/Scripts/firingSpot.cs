8d94da49f4c84d18fe0af47a5c0d38668b7e5a65
Anne <A.C.Anne.Chen@gmail.com>
2014-11-09 14:23:30 -0500

    The firing spot works. Successfully fires the chair to the other side of the plane.

--- Assets/Scripts/firingSpot.cs
+++ Assets/Scripts/firingSpot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class firingSpot : MonoBehaviour 
 {
-
+	public float speed;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,10 +17,10 @@ public class firingSpot : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		Vector3 movement = new Vector3(20.0f, 10.0f, 10.0f);
+		Vector3 movement = new Vector3(10.0f, 0.0f, 10.0f);
 		if(other.tag != "Player")
 		{
-			other.rigidbody.AddForce(movement * 50.0f * Time.deltaTime);
+			other.rigidbody.AddForce(movement * speed);
 		}
 	}
 }
