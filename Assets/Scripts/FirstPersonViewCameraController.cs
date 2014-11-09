0f175df9d51bfbdd12fdc178417ab4a60db43eda
ZhaoshengWang <wang.zhaosheng@hotmail.com>
2014-11-09 01:04:49 -0500

    Okay      let's see if this time it is working

---
+++ Assets/Scripts/FirstPersonViewCameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonViewCameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fowardSpeed = Input.GetAxis ("Vertical");
+		Vector3 speed = new Vector3 (0, 0, fowardSpeed);
+
+	
+	}
+}
