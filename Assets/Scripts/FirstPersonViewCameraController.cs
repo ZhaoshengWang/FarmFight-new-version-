8e5aa3d96d6cdbb3c798468f813572f33d9f6c69
ZhaoshengWang <wang.zhaosheng@hotmail.com>
2014-11-09 13:18:20 -0500

    try to pull this then add something and push

--- Assets/Scripts/FirstPersonViewCameraController.cs
+++ Assets/Scripts/FirstPersonViewCameraController.cs
@@ -12,16 +12,17 @@ public class FirstPersonViewCameraController : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 
-		float horizontalRotation = Input.GetAxis ("Mouse X") * 3.0f;
+		float horizontalRotation = Input.GetAxis ("Mouse Y") * 3.0f;
 		transform.Rotate (0, horizontalRotation, 0);
 		float verticalRotation = Input.GetAxis ("Mouse Y") * 3.0f;
 		verticalRotation = Mathf.Clamp (verticalRotation,-50.0f,50.0f);
-		//Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation,0,0);
+		transform.Rotate ( verticalRotation,0,0);
 		float leftRightSpeed = Input.GetAxis ("Horizontal")*10f;
 		float fowardSpeed = Input.GetAxis ("Vertical")*10f;
 
 		Vector3 movement = new Vector3 (leftRightSpeed, 0, fowardSpeed);
 		movement = transform.rotation * movement;
+
 		CharacterController cc = GetComponent<CharacterController> ();
 		cc.SimpleMove (movement);
 		//rigidbody.AddForce (movement * speed * Time.deltaTime);
