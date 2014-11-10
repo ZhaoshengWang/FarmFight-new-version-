4ccea07f4a3cc3a21abbcbea9c5650227adbf421
Anne <A.C.Anne.Chen@gmail.com>
2014-11-09 21:40:28 -0500

    Added the health bar to the player.

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+
+	bool damaged;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakeDamage(int amount)
+	{
+		damaged = true;
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+	}
+}
